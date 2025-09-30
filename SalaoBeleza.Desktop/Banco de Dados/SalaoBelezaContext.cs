5d581997b32e33838fc33ad832e45645c5684fe8
Gaaldra <gabrielalves.quadra@gmail.com>
2025-09-29 21:57:40 -0300

    Feat: Criado os modelos para o banco de dados

---
+++ SalaoBeleza.Desktop/Banco de Dados/SalaoBelezaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalaoBeleza.Desktop.Modelos;
+namespace SalaoBeleza.Desktop.Banco_de_Dados;
+
+internal class SalaoBelezaContext : DbContext
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string textoConexao = "server=localhost;user=root;password=;database=salaobeleza";
+        optionsBuilder.UseMySql(textoConexao, ServerVersion.AutoDetect(textoConexao));
+    }
+}
